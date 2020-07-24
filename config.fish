416c8b7915e914839088ba3ee48da6160088c910
Devin Ragotzy <devin.ragotzy@gmail.com>
2020-07-24 15:24:53 -0400

    Add dark-github and update fish for python

--- config.fish
+++ config.fish
@@ -9,3 +9,28 @@ alias l='exa'
 alias ls='exa -l'
 alias ll='exa -la'
 alias lt='exa -T'
+
+# PYTHON3 alias
+alias python='python3.8'
+alias pip=pip3
+
+# GIT status in the prompt
+set -g __fish_git_prompt_show_informative_status 1
+
+set -g __fish_git_prompt_color_branch magenta
+set -g __fish_git_prompt_showupstream "informative"
+set -g __fish_git_prompt_char_upstream_ahead "↑"
+set -g __fish_git_prompt_char_upstream_behind "↓"
+set -g __fish_git_prompt_char_upstream_prefix ""
+
+set -g __fish_git_prompt_char_stagedstate "🌴"
+set -g __fish_git_prompt_char_dirtystate "🐖"
+set -g __fish_git_prompt_char_untrackedfiles "🐿️"
+set -g __fish_git_prompt_char_conflictedstate "🐿️"
+set -g __fish_git_prompt_char_cleanstate "👌"
+
+set -g __fish_git_prompt_color_dirtystate yellow
+set -g __fish_git_prompt_color_stagedstate green
+set -g __fish_git_prompt_color_invalidstate red
+set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
+set -g __fish_git_prompt_color_cleanstate green
